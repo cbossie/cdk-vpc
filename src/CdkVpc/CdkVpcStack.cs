cbe72d8752986d23f3a6a1456084bebacfda7099
Craig Bossie <cbossie@gmail.com>
2020-08-22 21:16:07 -0400

    Initial Commit

---
+++ src/CdkVpc/CdkVpcStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace CdkVpc
+{
+    public class CdkVpcStack : Stack
+    {
+        internal CdkVpcStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
